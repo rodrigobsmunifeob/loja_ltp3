72a8a56958055aaf990781efef1b9718eae89a64
WLF <wendreoofernandes@gmail.com>
2017-05-02 20:33:21 -0300

    link to contact

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
             <li class="active"><a href="#">Home</a></li>
             <li><a href="#">Categorias</a></li>
             <li><a href="#">Carrinho</a></li>
-            <li><a href="#">Contato</a></li>
+            <li><a href="contact.php">Contato</a></li>
             <!-- 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nome do Cliente <span class="caret"></span></a>
