efa2c4a2561f736ee8d587fdf4b72c031e761c98
WLF <wendreoofernandes@gmail.com>
2017-05-16 22:40:31 -0300

    Refatorando Contato.php

--- contact.php
+++ contact.php
@@ -16,23 +16,25 @@ include("header.php");
 <fieldset><legend></legend>
 
 
-    <form action="../Controller/Register.php" method="post" class="form-signup-login w3-card-4 w3-container" name="formlogin">
+    <form action="#" method="post" class="form-signup-login w3-card-4 w3-container" name="formlogin">
 
 
         <div class="form-group box-login">
             <h4 style="text-align: center">Identificação do Usuario</h4>
-            <input type="text" id="input_css" name="name_user" id="txt_name" maxlength="15"  required placeholder="Nome ">
-            <input type="text" id="input_css" name="cpf_user" id="sobrenome" maxlength="15"  required placeholder="CPF" disabled>
+            <label for="txt_name">Nome</label>
+            <input style="width: 45% ;display: inline-block; margin-bottom: 15px;" type="text" class="form-control text-center" name="name_user" id="txt_name" value="<?php echo $_SESSION['nome']; ?>" required disabled>
+            <label for="txt_cpf">CPF</label>
+            <input style="width: 30%; display: inline-block;" type="text" class="form-control text-center" name="cpf_user" id="txt_cpf" value="<?php echo $_SESSION['cpf']; ?>" required disabled>
+            <label for="txt_datanasc">Logradouro</label>
+            <input style="width: 74% ;display: inline-block; margin-bottom: 15px;" type="text" class="form-control text-center" name="rua_user" id="txt_rua" value="<?php echo $_SESSION['end_rua']; ?>" required disabled>
+            <br><label for="txt_bairro">Bairro</label>
+            <input style="width: 30%; display: inline-block; margin-bottom: 15px;  " type="text" class="form-control text-center" name="bairro_user" id="txt_bairro" value="<?php echo $_SESSION['end_bairro']; ?>" required disabled>
+            <label for="txt_complemento">Complemento</label>
+            <input style="width: 25%; display: inline-block;" type="text" class="form-control text-center" name="complemento_user" id="txt_complemento" value="<?php echo $_SESSION['end_complemento']; ?>" required disabled>
+            <label for="txt_cidade">Cidade</label>
+            <input style="width: 50%; display: inline-block;" type="text" class="form-control text-center" name="cidade_user" id="txt_cidade" value="<?php echo $_SESSION['end_cidade']; ?>" required disabled>
 
-            <input type="date" id="input_css" name="rua_user" id="txt_datanasc" maxlength="8"  placeholder="Rua" required>
-
-
-            <input type="text" id="input_css" name="bairro_user" id="txt_name" maxlength="255"  required placeholder="Bairro">
-
-             <input type="text" id="input_css" name="complemento_user" id="txt_cidade" required  placeholder="Complemento">
-             <input type="text" id="input_css" name="cidade_user" id="txt_cidade" required  placeholder="Cidade">
-
-            <select name="estado_user" id="estado" style="min-width: 400px" >
+            <select name="estado_user" id="estado" style="height: 35px;border-radius: 6px ;width: 30%" disabled>
                 <option value="Acre">AC</option>
                 <option value="Alagoas" >AL</option>
                 <option value="Amapá" >AP</option>
@@ -43,7 +45,7 @@ include("header.php");
                 <option value="Espírito Santo" >ES</option>
                 <option value="Goiás" >GO</option>
                 <option value="Maranhão" >MA</option>
-                <option value="Mato Grosso" >MT</option>   
+                <option value="Mato Grosso" >MT</option>
                 <option value="Mato Grosso do Sul" >MS</option>
                 <option value="Minas Gerais" >MG</option>
                 <option value="Pará" >PA</option>
@@ -63,7 +65,9 @@ include("header.php");
                 </optgroup>
 
             </select>
-            <input type="text" id="input_css" name="email_user" id="txt_name"   placeholder="Email" disabled>
+            <label for="txt_email">Email</label>
+            <input style="width: 50%; display: inline-block;" type="text" class="form-control text-center" name="email_user" id="txt_email" value="<?php echo $_SESSION['email']; ?>"required disabled>
+
             <h4>Qual é sua dúvida ?</h4>
             <textarea class="form-control" id="input_css" rows="3" placeholder="Digite sua mensagem..."></textarea>
             <p class="text-center"><button type="submit" class="btn btn-success btn-lg" href="contact.php" role="button">Enviar</button>
